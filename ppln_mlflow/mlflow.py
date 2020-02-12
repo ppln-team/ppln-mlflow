cd69d0d7999b173de1bb7ab08e9d3ecafa2235bd
miras.amir <miras.amir@x5.ru>
2020-02-12 14:03:37 +0300

    [Feature] Add black & pre-commit-hooks

--- ppln_mlflow/mlflow.py
+++ ppln_mlflow/mlflow.py
@@ -20,24 +20,24 @@ class MlFlowLoggerHook(BaseLoggerHook):
     def log_lr(self, lr_dict):
         for optimizer_name, lrs in lr_dict.items():
             if len(lrs) == 1:
-                mlflow.log_metric(f'{optimizer_name}_lr', lrs[0])
+                mlflow.log_metric(f"{optimizer_name}_lr", lrs[0])
             else:
                 for i, lr in enumerate(lrs):
-                    mlflow.log_metric(f'{optimizer_name}_{i}_lr', lr)
+                    mlflow.log_metric(f"{optimizer_name}_{i}_lr", lr)
 
     @master_only
     def log(self, runner):
         for value in runner.log_buffer.output:
-            if value in ['time', 'data_time']:
+            if value in ["time", "data_time"]:
                 continue
-            tag = f'{runner.mode}_{value}'
+            tag = f"{runner.mode}_{value}"
             record = runner.log_buffer.output[value]
             if isinstance(record, str):
                 pass
             else:
                 mlflow.log_metric(tag, record, runner.epoch)
 
-            if runner.mode == 'train':
+            if runner.mode == "train":
                 lr_dict = get_lr(runner.optimizers)
                 self.log_lr(lr_dict)
 
